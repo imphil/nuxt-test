e5d0a7296a098ee8dfa7691d08f8a99715514b3f
Philipp Wagner <mail@philipp-wagner.com>
2023-01-16 18:26:25 +0100

    more test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+
+  content: { documentDriven: true },
+  // Auto-import components.
+  components: true,
+
+  // If you have enabled Take Over Mode or installed the TypeScript Vue Plugin
+  // (Volar), you can disable generating the shim for *.vue files in your
+  // nuxt.config file:
+  // https://nuxt.com/docs/getting-started/installation#prerequisites
+  typescript: {
+    shim: false
+  },
 })
